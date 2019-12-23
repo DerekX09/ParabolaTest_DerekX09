b18e93ef9e164520f09b4900b5367d475dcb15e6
Kevin Harrington <mad.hephaestus@gmail.com>
2019-12-23 10:49:13 -0500

    parabolic cone example

--- ParabolaTest.groovy
+++ ParabolaTest.groovy
@@ -4,6 +4,5 @@ return [ Parabola.extrudeByEquation(5,0.27,0,1)
 	.move(30,0,0),
 	Parabola.coneByFocalLength(10, 10),
 	Parabola.coneByHeight(10, 20)
-	.rotx(90).toZMin()
-	.move(50,0,0)
+		.move(50,0,0)
 ]

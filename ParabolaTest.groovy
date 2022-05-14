93a4275b4a06df3cf82278d4e8080500628aeab4
Kevin Harrington <mad.hephaestus@gmail.com>
2022-05-14 14:44:24 -0400

    adding better examples

--- ParabolaTest.groovy
+++ ParabolaTest.groovy
@@ -5,5 +5,6 @@ return [ Parabola.extrudeByEquation(5,0.27,0,1)
 	Parabola.coneByEquation(5,1.27,0.7)
 	.move(30,0,0),
 	Parabola.coneByFocalLength(10, 10).move(50,0,0),
-	Parabola.cone(10, 20)		
+	Parabola.cone(10, 20), // smooth parabolic cone
+	Parabola.cone(10, 60,3).movex(-30)// Pointy parabolic cone		
 ]

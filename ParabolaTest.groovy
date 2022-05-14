9b099252aeea12e146e788285d8f0ca827525f1b
Kevin Harrington <mad.hephaestus@gmail.com>
2022-05-14 14:33:21 -0400

    updating the demo

--- ParabolaTest.groovy
+++ ParabolaTest.groovy
@@ -4,7 +4,6 @@ return [ Parabola.extrudeByEquation(5,0.27,0,1)
 	.move(20,0,0),
 	Parabola.coneByEquation(5,1.27,0.7)
 	.move(30,0,0),
-	Parabola.coneByFocalLength(10, 10),
-	Parabola.coneByHeight(10, 20)
-		.move(50,0,0)
+	Parabola.coneByFocalLength(10, 10).move(50,0,0),
+	Parabola.cone(10, 20)		
 ]
